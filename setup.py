07283f7ea8f5536babdcb37b5de2d4e79d9a1691
kalinkochnev <kalin.kochnev@gmail.com>
2020-02-23 13:50:00 -0500

    setup.py: removed unnecessary requirements

--- setup.py
+++ setup.py
@@ -11,14 +11,7 @@ setup(
 
     # https://setuptools.readthedocs.io/en/latest/setuptools.html#declaring-dependencies
     install_requires=[
-        'Click==7.0',
-        'Flask==1.1.1',
-        'itsdangerous==1.1.0',
-        'Jinja2==2.11.1',
-        'MarkupSafe==1.1.1',
-        'numpy==1.16.4',
         'psutil==5.6.7',
-        'Werkzeug==0.16.0',
         'pytest==5.3.5',
         'pathlib2==2.3.5'
     ],
